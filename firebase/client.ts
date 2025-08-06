b4feaff043f609c5491c8b3a8d428aec6d7901df
Kenny Nguyen <73307872+kennydn99@users.noreply.github.com>
2025-08-05 23:31:06 -0700

    Setup firebase - firestore and fireauth

---
+++ firebase/client.ts
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase-admin/auth";
+import { getFirestore } from "firebase-admin/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAulna8FjPntOpvKn3ixUCRO8RXkz5UpsA",
+  authDomain: "mockmate-b16df.firebaseapp.com",
+  projectId: "mockmate-b16df",
+  storageBucket: "mockmate-b16df.firebasestorage.app",
+  messagingSenderId: "406633179905",
+  appId: "1:406633179905:web:c3c6ca4206ff7ca268c900",
+  measurementId: "G-GZ8NNPBMMD",
+};
+
+// Initialize Firebase
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
